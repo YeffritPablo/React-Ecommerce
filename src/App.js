e4d3512ea73177b84e96aed227bfdba721a6af2b
becker.pablo <yeffribecker27@hotmail.com>
2022-06-06 14:06:45 -0300

    Se agrega el primer proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './componentes/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+   <Navbar />
+        <h1>Mi Ecommerce</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          by Becker Pablo 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+    
   );
-}
+};
 
 export default App;
