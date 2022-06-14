85e42e1a0642b4d165fc9d3762c2d5cdf00874b5
YeffritPablo <95254442+YeffritPablo@users.noreply.github.com>
2022-06-14 19:25:44 -0300

    Se agrego contador

--- src/componentes/ItemCount.js
+++ src/componentes/ItemCount.js
@@ -3,13 +3,13 @@ import '../componentes/Header/Navbar.css';
 import React, {useState} from "react";
 const suma = document.querySelector('#sum')
 const dec = document.querySelector('#dec')
-const ItemCount =({img})=>{
+const ItemCount =()=>{
     const [count,setCount] = useState(0);
     const aumentador =()=>{
        setCount(count + 1);
         if(count >= 9){
 
-            suma.classList.toggle('none')
+            suma.classList.add('none')
        
         }else if(count>=0){
             dec.classList.remove('none')
@@ -20,7 +20,7 @@ const ItemCount =({img})=>{
 const sacar = ()=>{
     setCount(count - 1);
     if(count <= 1){
-        dec.classList.toggle('none')
+        dec.classList.add('none')
     }else if (count>=10){
         suma.classList.remove('none')
     }
@@ -28,8 +28,6 @@ const sacar = ()=>{
 
     return(
         <div>
-            <p>Mouse</p>
-            <img src={img} alt="Raton"></img>
             <p>{count}</p>
             <button id="sum" onClick={aumentador}>aumentador</button>
             <button id="dec" onClick={sacar}>Sacar</button>
